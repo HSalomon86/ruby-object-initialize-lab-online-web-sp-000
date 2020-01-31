f1814d0e1da1c54d36c59b14ddef66aeab012d38
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-31 03:19:29 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,12 +2,14 @@ class Person
   def initialize(name)
     @name = name
   end
-
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
+  attr_reader :name
+  attr_writer :name
+  #
+  # def name=(name)
+  #   @name = name
+  # end
+  #
+  # def name
+  #   @name
+  # end
 end
